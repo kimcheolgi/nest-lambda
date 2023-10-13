129ab2f4475ec02b608c85410ac2f796317bcc56
kimcheolgi <kimchelgi03@gmail.com>
2023-10-13 16:07:30 +0900

    feat: lambda에서 swagger 접근 세팅

--- src/lambda.ts
+++ src/lambda.ts
@@ -2,13 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('The is a sample REST API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.init();
-
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
