326f28b84865944aafda271383e8a85158d02663
kimcheolgi <kimchelgi03@gmail.com>
2023-10-13 15:36:51 +0900

    feat: user crud 추가 및 swagger 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
